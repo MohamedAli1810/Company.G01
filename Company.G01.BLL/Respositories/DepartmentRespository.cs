927337732321c97c4746fd51aa2f09072005392a
MohamedAli1810 <mohamed.eloghaazi@gmail.com>
2025-03-09 22:55:56 +0200

    Depatment Module BLL

---
+++ Company.G01.BLL/Respositories/DepartmentRespository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G01.DAL.Data.Contexts;
+using Company.G01.DAL.Models;
+
+namespace Company.G01.BLL.Respositories
+{
+    public class DepartmentRespository : IDepartmentRespository
+    {
+        private CompanyDbContext _Context;
+
+        public DepartmentRespository()
+        {
+            _Context = new CompanyDbContext();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _Context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+            return _Context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+             _Context.Departments.Add(model);
+            return _Context.SaveChanges();
+        }
+        
+        public int Update(Department model)
+        {
+            _Context.Departments.Update(model);
+            return _Context.SaveChanges();
+        }
+        public int Delete(Department model)
+        {
+            _Context.Departments.Remove(model);
+            return _Context.SaveChanges();
+        }
+    }
+}
