ab56f356e0e6a4f3223f6eba503817ac9e411394
MohamedAli1810 <mohamed.eloghaazi@gmail.com>
2025-03-17 12:12:37 +0200

    Implementation of BLL for Employee services

--- Company.G01.BLL/Respositories/DepartmentRespository.cs
+++ Company.G01.BLL/Respositories/DepartmentRespository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Company.G01.BLL.Interfaces;
 using Company.G01.DAL.Data.Contexts;
 using Company.G01.DAL.Models;
 
